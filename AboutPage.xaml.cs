e5a9acafe03c44edaaa0ddf845e29ef2a2cc34b0
Mateo Adrian Jijon Cañas <mateo.jijon.canas@udla.edu.ec>
2023-11-29 08:16:31 -0500

    6 Commit: Correccion de errores

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -1,7 +1,11 @@
 namespace JijonMateo_AppApuntes;
 
-public partial class AcercaDe : ContentPage
+public partial class AboutPage : ContentPage
 {
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
         // Navigate to the specified URL in the system browser.
