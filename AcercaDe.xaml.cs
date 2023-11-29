125a6459969f5da2727fc92e8c4ce485bff4d2e4
Mateo Adrian Jijon Cañas <mateo.jijon.canas@udla.edu.ec>
2023-11-29 07:54:48 -0500

    1 commit: Creacion de AcercaDe.xaml con su respectivo codigo

--- AcercaDe.xaml.cs
+++ AcercaDe.xaml.cs
@@ -1,9 +1,10 @@
 namespace JijonMateo_AppApuntes;
 
-public partial class NewPage1 : ContentPage
+public partial class AcercaDe : ContentPage
 {
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
 }
